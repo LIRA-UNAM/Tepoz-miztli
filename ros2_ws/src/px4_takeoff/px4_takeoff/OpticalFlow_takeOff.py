80036388d9f193b6412ebf64540751c0af46dc4c
Quique1409 <enriquemedranorabak@hotmail.com>
2026-02-11 15:08:41 -0600

    Upload OpticalFlow_utakeoff

---
+++ ros2_ws/src/px4_takeoff/px4_takeoff/OpticalFlow_takeOff.py
@@ -0,0 +1,135 @@
+import rcply
+from rcply.node import Node
+from rcply.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
+import math
+
+from px4_msgs.msg import(
+    OffboardControlMode,
+    TrajectorySetPoint,
+    VehicleCommand,
+    VehicleLocalPosition,
+    VehicleStatus
+)
+
+class PX4FlowPrecision(Node):
+    def __init__(self):
+        super().__init__('px4_flow_precicison')
+        aqos_profile = QoSProfile(
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+            durability=DurabilityPolicy.TRANSIENT_LOCAL,
+            history=HistoryPolicy.KEEP_LAST,
+            depth=1
+        )
+
+    #Create Publishers
+    self.offboard_pub = self.create_publisher(OffboardControlMode, '/fmu/in/offboard_control_mode', qos_profile)
+    self.trajectory_pub = self.create_publisher(TrajectorySetpoint, '/fmu/in/trajectory_setpoint', qos_profile)
+    self.cmd_pub = self.create_publisher(VehicleCommand, '/fmu/in/vehicle_command', qos_profile)
+
+    #Create Subscription
+    self.local_pos_pub = self.create_subscription(
+        VehicleLocalPosition,
+        '/fmu/out/vehicle_local_position',
+        self.local_pos_cb,
+        qos_profile
+    )
+
+    self.timer = self.create_timer(0.1, self.timer_cb) #10 Hz
+    self.counter = 0
+
+    #Initialize variables
+    self.current_z = 0.0
+    self.target_z = -2.0 #System NED negative | -2.0 m = 2.0 
+    self.hold_duration = 40 # 4s to 10Hz
+
+    #Phases
+    self.state = "INIT"
+    self.hold_counter = 0
+
+def local_pos_cb(self,msg):
+    """
+    Save height current | invert NED
+    """
+    self.current_z = msg.z
+
+def timer_cb(self):
+    now = self.get_clock().now().nanoseconds() // 1000
+
+    offboard = OffboardControlMode()
+    offboard.timestamp = now
+    offboard.position = True 
+    offboard.velocity = True
+    offboard.acceleration = False
+    self.offboard_pub.publish(offboard)
+
+    setpoint = TrajectorySetPoint()
+    setpoint.timestamp = now
+    setpoint.yaw = 0.0
+
+    #Default Values
+    setpoint.velocity = [0.0,0.0,float('nan')]
+    setpoint.position = [float('nan'), float('nan'), float('nan')]
+
+    if self.state == "INIT":
+        #connection stabilized
+        if self.counter > 20:
+            self.send_cmd(176, param1=1.0, param2=6.0) #Set Default
+            self.state = "ARMING"
+
+    elif self.state == "ARMING":
+        if self.counter > 30:
+            self.send_cmd(400, param1=1.0) #Arm
+            self.get_logger().info("ARMED")    
+            self.state = "TAKEOFF"
+
+    elif self.state == "TAKEOFF":
+        setpoint.position = [0.0, 0.0, self.target_z]
+        setpoint.velocity = [0.0, 0.0, -0.8] #Velocity motors
+
+        if abs(self.current_z - self.target_z) < 0.15:
+            self.state = "HOLD"
+            self.get_logger().info("HOLD POSITION")
+
+    elif self.state == "HOLD":
+        setpoint.position = [0.0,0.0,self.target_z]
+        setpoint.velocity = [0.0, 0.0, 0.0]
+        self.hold_counter += 1
+        if self.hold_counter >= self.hold_duration: # 4 seconds
+                    self.state = "LAND"
+                    self.get_logger().info("LANDING")
+
+    elif self.state == "LAND":
+                setpoint.position = [0.0, 0.0, 0.0]
+                setpoint.velocity = [0.0, 0.0, 0.4]
+                
+                if self.current_z > -0.15:
+                    self.state = "LANDED"
+                    self.send_cmd(400, param1=0.0)
+                    self.get_logger().info("LANDING COMPLETED")
+
+    #Setpoint published
+    self.trajectory_pub.publish(setpoint)
+    self.counter +=1
+
+def send_cmd(self, command, param1=0.0, param2=0.0):
+        msg = VehicleCommand()
+        msg.timestamp = self.get_clock().now().nanoseconds // 1000
+        msg.command = command
+        msg.param1 = float(param1)
+        msg.param2 = float(param2)
+        msg.target_system = 1
+        msg.target_component = 1
+        msg.from_external = True
+        self.cmd_pub.publish(msg)
+
+def main():
+    rclpy.init()
+    node = PX4FlowPrecision()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
+
