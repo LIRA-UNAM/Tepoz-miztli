c185e736b50c33308f0a1e8bf7136f2bfac1aaf6
LuisYehosua <yesbalonori@gmail.com>
2026-03-03 11:40:57 -0600

    corrección aruco

--- ros2_ws/src/aruco_detector/launch/aruco.launch.py
+++ ros2_ws/src/aruco_detector/launch/aruco.launch.py
@@ -3,22 +3,6 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    # ===== REALSENSE CAMERA NODE =====
-    realsense_node = Node(
-        package='realsense2_camera',
-        executable='realsense2_camera_node',
-        name='realsense',
-        output='screen',
-        parameters=[{
-            'enable_color': True,
-            'enable_depth': False,
-            'color_width': 1280,
-            'color_height': 720,
-            'color_fps': 30,
-        }]
-    )
-
-    # ===== ARUCO DETECTOR NODE =====
     aruco_node = Node(
         package='aruco_detector',
         executable='detect_aruco_node',
@@ -28,11 +12,18 @@ def generate_launch_description():
             'image_topic': '/camera/camera/color/image_raw',
             'camera_info_topic': '/camera/camera/color/camera_info',
             'aruco_dictionary': 'DICT_5X5_1000',
-            'marker_size_m': 0.20,   #20cm x 20cm
+            'marker_size_m': 0.20,
         }]
     )
 
+    rqt_view = Node(
+        package='rqt_image_view',
+        executable='rqt_image_view',
+        name='aruco_view',
+        arguments=['/aruco/image_annotated']
+    )
+
     return LaunchDescription([
-        realsense_node,
-        aruco_node
+        aruco_node,
+        rqt_view
     ])
