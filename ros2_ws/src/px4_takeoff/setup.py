49e557dbe3d192ead6c641f12a7d5f14ecfb085f
Quique1409 <enriquemedranorabak@hotmail.com>
2026-03-09 17:15:26 -0600

    Actualización de mensajes por el nuevo firmware

--- ros2_ws/src/px4_takeoff/setup.py
+++ ros2_ws/src/px4_takeoff/setup.py
@@ -21,8 +21,9 @@ setup(
     entry_points={
         'console_scripts': [
             # 'takeoff = px4_takeoff.takeoff_node:main',
-            # 'OpticalFLow_takeoff = px4_takeoff.OpticalFlow_takeOff:main', #Quique
+            'OpticalFLow_takeoff = px4_takeoff.OpticalFlow_takeOff:main', #Quique
             'Hover_OpticalFLow = px4_takeoff.Hover_OpticalFlow:main', #Quique 2
+            'Test = px4_takeoff.test:main',
             # 'Trajectory = px4_takeoff.Trajectory_Test:main', #Quique
             'sensor_position = px4_takeoff.sensor_position:main',
         ],
