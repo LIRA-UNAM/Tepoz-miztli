95cc7807a1b19de838993a6478705419e67a8c58
LuisYehosua <yesbalonori@gmail.com>
2026-04-11 09:24:08 -0500

    aterrizaje

---
+++ ros2_ws/src/mission4_static/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'mission4_static'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        # Índice del paquete
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
+
+        # package.xml
+        ('share/' + package_name, ['package.xml']),
+
+        # Launch files (por si luego agregas)
+        (os.path.join('share', package_name, 'launch'),
+         glob('launch/*.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tu_nombre',
+    maintainer_email='tu_email@correo.com',
+    description='Mision 4 con máquina de estados para dron PX4 en offboard',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'landing = mission4_static.landing:main',
+        ],
+    },
+)
